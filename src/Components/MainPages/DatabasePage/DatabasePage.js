4ac97db303da4b86cb39835450ea5c094e3c6c01
Георгий Шахов <g.shakhov@wisetech.pro>
2026-06-14 23:46:27 +0200

    added styles for DatabagePage

--- src/Components/MainPages/DatabasePage/DatabasePage.js
+++ src/Components/MainPages/DatabasePage/DatabasePage.js
@@ -3,8 +3,10 @@ import { DatabaseItems } from "Components/MainPages/DatabasePage/Items/DatabaseI
 import { NavButton } from "Components/Navigation/NavButton";
 import * as Nav from "Components/Navigation/paths.js";
 
+
 import "Styles/Navigation/NavButton.scss";
 import "Styles/CenteredText.scss";
+import "Styles/MainPages/DatabasePage/DatabasePage.scss";
 
 export const DatabasePage = () => {
 	const { data, error, isLoading } = usePrivateCatalogItems();
@@ -20,9 +22,9 @@ export const DatabasePage = () => {
 	const items = data?.data ?? [];
 
 	return (
-		<>
+		<div className="database-page">
 			<NavButton labelText="Add new" destination={`/${Nav.upload}`} />
 			<DatabaseItems catalogState={items} />
-		</>
+		</div>
 	);
 };
