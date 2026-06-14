bdda38ed40fa76bf1b5701e7ab440f4b4dc1a9dc
condemnJS <55482290+condemnJS@users.noreply.github.com>
2026-06-14 17:02:10 +0200

    Change base server URL to HTTPS
    Updated base server URL to use HTTPS.

--- src/Constants.js
+++ src/Constants.js
@@ -15,7 +15,7 @@ export const type = "typeId"
 export const status = "status"
 export const description = "description"
 
-export const base_server_url = "http://api.garbage-garage.com/"
+export const base_server_url = "https://api.garbage-garage.com/"
 
 // API для загрузки вещи на сервер
 export const post_upload = "post/upload"
