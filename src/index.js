f846c91409d1741f269835885571fdd204591ab7
condemnJS <55482290+condemnJS@users.noreply.github.com>
2026-06-17 00:11:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
+console.log('test log');
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 	  <QueryClientProvider client={queryClient}>
     <App />
