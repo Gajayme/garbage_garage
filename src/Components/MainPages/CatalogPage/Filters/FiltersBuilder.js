dbca9f6ba578df0ddbf39312765a1e9e0766c8ab
Георгий Шахов <g.shakhov@wisetech.pro>
2026-08-15 19:51:21 +0200

    redisigned filters window

--- src/Components/MainPages/CatalogPage/Filters/FiltersBuilder.js
+++ src/Components/MainPages/CatalogPage/Filters/FiltersBuilder.js
@@ -14,53 +14,53 @@ import 'Styles/MainPages/CatalogPage/Filters/FiltersContent.scss'
 
 
 export const FilterBuilder = ({
-  availableFilters,
-  filtersState,
-  toggleFilterVisibility,
-  onFilterStateChanged,
-  filtersVisibility
+	availableFilters,
+	filtersState,
+	toggleFilterVisibility,
+	onFilterStateChanged,
+	filtersVisibility
 }) => {
-  const renderFilter = (filterData) => {
-    switch (filterData.type) {
-      case Constants.FilterType.multiCheckbox:
-        return (
-          <CheckboxMultiFilter
-            allValues={filterData.values}
-            checkedOptions={filtersState[filterData.name] || []}
-            onChange={onFilterStateChanged(filterData.name)}
-            checkmarkImg={checkmark}
-          />
-        );
-      case Constants.FilterType.range:
-        return (
-          <RangeFilter
-            image={rangeArrow}
-			onChange={onFilterStateChanged(filterData.name)}
-            currentValues={filtersState[filterData.name] || { min: '', max: '' }}
-          />
-        );
-      default:
-        return null;
-    }
-  };
+	const renderFilter = (filterData) => {
+		switch (filterData.type) {
+			case Constants.FilterType.multiCheckbox:
+				return (
+					<CheckboxMultiFilter
+						allValues={filterData.values}
+						checkedOptions={filtersState[filterData.name] || []}
+						onChange={onFilterStateChanged(filterData.name)}
+						checkmarkImg={checkmark}
+					/>
+				);
+			case Constants.FilterType.range:
+				return (
+					<RangeFilter
+						image={rangeArrow}
+						onChange={onFilterStateChanged(filterData.name)}
+						currentValues={filtersState[filterData.name] || { min: '', max: '' }}
+					/>
+				);
+			default:
+				return null;
+		}
+	};
 
-  return (
-    <div className="filters-content">
-      {availableFilters.map((filterData) => (
-        <FilterWithButton
-          key={filterData.name}
-          filter={renderFilter(filterData)}
-          labelText={filterData.name}
-          className="filter-with-button"
-          buttonClassName="filter-button"
-          iconClassName="filter-arrow-icon"
-          iconInactive={arrowDown}
-		  iconActive={arrowUp}
-          onClick={() => toggleFilterVisibility(filterData.name)}
-          altImg={filterData.name}
-          isActive={filtersVisibility[filterData.name]}
-        />
-      ))}
-    </div>
-  );
+	return (
+		<div className="filters-content">
+			{availableFilters.map((filterData) => (
+				<FilterWithButton
+					key={filterData.name}
+					filter={renderFilter(filterData)}
+					labelText={filterData.name}
+					className="filter-with-button"
+					buttonClassName="filter-button"
+					iconClassName="filter-arrow-icon"
+					iconInactive={arrowDown}
+					iconActive={arrowUp}
+					onClick={() => toggleFilterVisibility(filterData.name)}
+					altImg={filterData.name}
+					isActive={filtersVisibility[filterData.name]}
+				/>
+			))}
+		</div>
+	);
 };
